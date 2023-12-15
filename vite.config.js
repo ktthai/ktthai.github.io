196c5280fe66fe337120610329312c9ce205d1db
XanderKing <kenneth.lam1997@hotmail.com>
2023-12-15 10:56:53 -0500

    Changed structure for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: '../dist'
-  },
-  plugins: [nodePolyfills()]
+  plugins: [nodePolyfills()],
+  base: "/MabiPlayerLogger/"
 })
