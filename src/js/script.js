a4244c8849f6f4292bed9bd4211e52ecc7bdf543
XanderKing <kenneth.lam1997@hotmail.com>
2023-12-15 02:29:37 -0500

    Added chart js and all chart types to page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-import * as d3 from "d3";
 import $ from "jquery";
 window.jQuery = $;
 window.$ = $;
@@ -6,6 +5,7 @@ global.jQuery = $;
 
 import "bootstrap";
 import "bootstrap-table";
+import Chart from 'chart.js/auto';
 
 async function parseDataTable(url) {
     const response = await fetch(url);
@@ -27,6 +27,8 @@ async function parseDataTable(url) {
         let row = {};
         let text = rawData[i].split(",");
 
+        if (text.includes("")) continue;
+
         // Generating dicts for every row.
         text.forEach((val, j) => {
             val = val.trim();
@@ -34,6 +36,8 @@ async function parseDataTable(url) {
             if (headers[j] === "Race") {
                 val = val.split(" ");
                 const [gender, race] = val;
+
+                if (gender === "" || race === "") return;
 
                 // Bin based on race then gender.
                 if (!raceCounter[race]) {
@@ -52,7 +56,7 @@ async function parseDataTable(url) {
                 row["Race"] = race;
             }
             else {
-                if (headers[j] === "Server") {
+                if (headers[j] === "Server" && val !== "") {
                     serverCounter[val] = serverCounter[val] ? serverCounter[val] + 1 : 1;
                 }
                 else if (headers[j] === "Total Level") {
@@ -68,8 +72,6 @@ async function parseDataTable(url) {
             }
         });
 
-        if (Object.values(row).includes("")) continue;
-
         data.push(row);
     }
 
@@ -81,11 +83,154 @@ async function parseDataTable(url) {
     };
 }
 
+function buildServerChart(data) {
+    const serverTotal = Object.values(data).reduce((a, b) => a + b, 0);
+
+    new Chart(
+        document.getElementById("serverData"),
+        {
+            type: "bar",
+            data: {
+                datasets: [{ data: data }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        callbacks: {
+                            footer: function(tooltipItems) {
+                                const y = tooltipItems[0].parsed.y;
+                                const percentage = Math.round(y / serverTotal * 10000) / 100;
+                                return "(" + percentage + "%)";
+                            }
+                        }
+                    }
+                },
+                scales: {
+                    x: { title: { display: true, text: "Server" }},
+                    y: { title: { display: true, text: "Player Count" }}
+                }
+            }
+        }
+    );
+}
+
+function buildRaceChart(data) {
+    const raceTotal = {};
+    const genderTotal = {};
+
+    for (const [race, x] of Object.entries(data)) {
+        for (const [gender, y] of Object.entries(x)) {
+            raceTotal[race] = raceTotal[race] ? raceTotal[race] + y : y;
+            genderTotal[gender] = genderTotal[gender] ? genderTotal[gender] + y: y;
+        }
+    }
+
+    const serverTotal = Object.values(raceTotal).reduce((a, b) => a + b, 0);
+
+    new Chart(
+        document.getElementById("raceData"),
+        {
+            type: "bar",
+            data: {
+                labels: ["Male", "Female"],
+                datasets: [{
+                    label: "Human",
+                    data: data["Human"]
+                },
+                {
+                    label: "Elf",
+                    data: data["Elf"]
+                },
+                {
+                    label: "Giant",
+                    data: data["Giant"]
+                }]
+            },
+            options: {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            footer: function(tooltipItems) {
+                                const gender = tooltipItems[0].label;
+                                const race = tooltipItems[0].dataset.label;
+                                const count = tooltipItems[0].parsed.y;
+
+                                const genderPercent = Math.round(count / genderTotal[gender] * 10000) / 100;
+                                const racePercent = Math.round(count / raceTotal[race] * 10000) / 100;
+                                const totalPercent = Math.round(count / serverTotal * 10000) / 100;
+
+                                let tooltip = "(" + genderPercent + "% of " + genderTotal[gender] + " " + gender + "s)\n";
+                                tooltip += "(" + racePercent + "% of " + raceTotal[race] + " " + race + "s)\n";
+                                tooltip += "(" + totalPercent + "% of " + serverTotal + " Players)";
+
+                                return tooltip;
+                            }
+                        }
+                    }
+                },
+                scales: {
+                    x: { 
+                        title: { display: true, text: "Gender" },
+                        stacked: true
+                    },
+                    y: { 
+                        title: { display: true, text: "Player Count" },
+                        stacked: true
+                    }
+                }
+            }
+        }
+    );
+}
+
 $(async () => {
     const url = 'https://raw.githubusercontent.com/ktthai/ktthai.github.io/main/PlayerData.csv';
     const dataObj = await parseDataTable(url);
 
-    $("#csvData").bootstrapTable({
+    console.log(dataObj);
+    buildServerChart(dataObj.serverCounter);
+    buildRaceChart(dataObj.raceCounter);
+
+    const serverTotal = Object.values(dataObj.levelCounter).reduce((a, b) => a + b, 0);
+
+    new Chart(
+        document.getElementById("levelData"),
+        {
+            type: "bar",
+            data: {
+                datasets: [{ data: dataObj.levelCounter }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        callbacks: {
+                            footer: function(tooltipItems) {
+                                const y = tooltipItems[0].parsed.y;
+                                const percentage = Math.round(y / serverTotal * 10000) / 100;
+                                return "(" + percentage + "%)";
+                            }
+                        }
+                    }
+                },
+                scales: {
+                    x: { title: { display: true, text: "Total Level (x1000)" }},
+                    y: {
+                        title: { display: true, text: "Player Count" }, 
+                        // type: "logarithmic",
+                        // min: 0.9
+                    }
+                }
+            }
+        }
+    );
+
+    $("#playerData").bootstrapTable({
         columns: [
             {field: "IGN", title: "Name", sortable: true},
             {field: "Server", title: "Server", sortable: true},
